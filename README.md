291dc7c543206043218e63360adf90e235305c5a
GitJonibek <jonibeknorboev@gmail.com>
2024-03-29 13:06:20 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# GPT4ALL-Voice-Assistant
-This is a 100% offline GPT4ALL Voice Assistant. Completely open source and privacy friendly. Use any language model on GPT4ALL. Background process voice detection. Watch the full YouTube tutorial for setup guide: https://youtu.be/6zAk0KHmiGw
+# Friday AI
+This is a 100% offline AI Voice Assistant. Completely open source and privacy friendly. Use any language model on GPT4ALL. 
+
 ## Setup
-I highly advise watching the YouTube tutorial to use this code. You will need to modify the OpenAI whisper library to work offline and I walk through that in the video as well as setting up all the other dependencies to function properly. 
+1. Install all dependencies from `requirements.txt` file
+2. Install `ffmpeg`:
+   On Windows: `choco install ffmpeg`
+   On MacOS: `brew install ffmpeg`
+   on Linux: `sudo apt update && sudo apt install ffmpeg`
+
+   If you are on MacOS, you need to install `portaudio` library: Run `brew install portaudio` 
+3. And finally run `python3 main.py`
+
+I highly advise you to use Deepgram or alternative Text-To-Speech services for more human-like natural voices.
+
 ## Improvements to think about adding to yours
 Give a system prompt. These open source models perform far better when you send a system prompt as specified in the GPT4ALL documentation: https://docs.gpt4all.io/gpt4all_python.html#introspection
--- main.py
+++ main.py
@@ -1,82 +1,102 @@
-from os import system
-import speech_recognition as sr
-from gpt4all import GPT4All
 import sys
-import whisper
-import warnings
-import time
-import os
-
-wake_word = 'jarvis'
-model = GPT4All("/Users/YOUR_USERNAME_HERE/Library/Application Support/nomic.ai/GPT4All/ggml-model-gpt4all-falcon-q4_0.bin", allow_download=False)
-r = sr.Recognizer()
-tiny_model_path = os.path.expanduser('~/.cache/whisper/tiny.pt')
-base_model_path = os.path.expanduser('~/.cache/whisper/base.pt')
-tiny_model = whisper.load_model(tiny_model_path)
-base_model = whisper.load_model(base_model_path)
-listening_for_wake_word = True
-source = sr.Microphone() 
-warnings.filterwarnings("ignore", category=UserWarning, module='whisper.transcribe', lineno=114)
+from gpt4all import GPT4All
+import speech_recognition as sr
 
 if sys.platform != 'darwin':
     import pyttsx3
     engine = pyttsx3.init() 
 
+
+model_name = 'ggml-model-gpt4all-falcon-q4_0.bin'
+# model = GPT4All(model_name, allow_download=True) ==> run this to download language model at first.
+model_path = r'C:\Users\jonib\.cache\gpt4all'
+model = GPT4All(model_name, model_path, allow_download=False)
+
+voices = engine.getProperty('voices') 
+mic = sr.Microphone()
+recognizer = sr.Recognizer()
+
+transcribing = False
+mic_muted = True
+wake_word = "friday"
+
+def recognize_speech():
+    global mic_muted
+    global wake_word
+    with mic as source:
+        print("Listening for commands...")
+        recognizer.adjust_for_ambient_noise(source)
+
+        while True:
+            try:
+                audio = recognizer.listen(source)
+
+                transcription = recognizer.recognize_google(audio)
+
+                if not mic_muted:
+                    print('You: ' + transcription)
+                    output = model.generate(transcription, max_tokens=200)
+                    print('GPT4All: ', output)
+                    speak(output)
+
+                if wake_word in transcription.lower():
+                    start_transcription()
+                elif "mute" in transcription.lower() or "stop" in transcription.lower() or "stop talking" in transcription.lower() or "shut up" in transcription.lower():
+                    stop_transcription()
+                elif "go to sleep" in transcription.lower():
+                    print("Application is going to sleep.")
+                    sys.exit()
+                
+
+            except sr.UnknownValueError:
+                print("Sorry, I could not understand what you said.")
+
+            except sr.RequestError as e:
+                print("Could not request results; {0}".format(e))
+
 def speak(text):
+    global mic_muted
+
     if sys.platform == 'darwin':
         ALLOWED_CHARS = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,?!-_$:+-/ ")
         clean_text = ''.join(c for c in text if c in ALLOWED_CHARS)
         system(f"say '{clean_text}'")
     else:
-        engine.say(text)
-        engine.runAndWait()
+        engine.setProperty('rate', 160)
+        engine.setProperty('volume', 0.9)
+        engine.setProperty('voice', voices[1].id)
 
-def listen_for_wake_word(audio):
-    global listening_for_wake_word
-    with open("wake_detect.wav", "wb") as f:
-        f.write(audio.get_wav_data())
-    result = tiny_model.transcribe('wake_detect.wav')
-    text_input = result['text']
-    if wake_word in text_input.lower().strip():
-        print("Wake word detected. Please speak your prompt to GPT4All.")
-        speak('Listening')
-        listening_for_wake_word = False
+        if not mic_muted:
+            turn_off_microphone()
+            engine.say(text)
+            engine.runAndWait()
+            turn_on_microphone()
 
-def prompt_gpt(audio):
-    global listening_for_wake_word
-    try:
-        with open("prompt.wav", "wb") as f:
-            f.write(audio.get_wav_data())
-        result = base_model.transcribe('prompt.wav')
-        prompt_text = result['text']
-        if len(prompt_text.strip()) == 0:
-            print("Empty prompt. Please speak again.")
-            speak("Empty prompt. Please speak again.")
-            listening_for_wake_word = True
-        else:
-            print('User: ' + prompt_text)
-            output = model.generate(prompt_text, max_tokens=200)
-            print('GPT4All: ', output)
-            speak(output)
-            print('\nSay', wake_word, 'to wake me up. \n')
-            listening_for_wake_word = True
-    except Exception as e:
-        print("Prompt error: ", e)
+def turn_on_microphone():
+    global mic_muted
+    mic_muted = False
 
-def callback(recognizer, audio):
-    global listening_for_wake_word
-    if listening_for_wake_word:
-        listen_for_wake_word(audio)
-    else:
-        prompt_gpt(audio)
+def turn_off_microphone():
+    global mic_muted
+    mic_muted = True
 
-def start_listening():
-    with source as s:
-        r.adjust_for_ambient_noise(s, duration=2)
-    print('\nSay', wake_word, 'to wake me up. \n')
-    r.listen_in_background(source, callback)
-    while True:
-        time.sleep(1) 
+def start_transcription():
+    global transcribing
+    transcribing = True
+    turn_on_microphone()
+    
+    print("Listening...")
+    speak("Hi... I am listening.")
 
-if __name__ == '__main__':
-    start_listening() 
+def stop_transcription():
+    global transcribing
+    transcribing = False
+    turn_off_microphone()
+
+    print("Microphone is on mute!")
+
+def main():
+    recognize_speech()
+
+if __name__ == "__main__":
+    main()
